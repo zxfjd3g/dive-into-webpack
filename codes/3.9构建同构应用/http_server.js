cd4cb867bff32487a883f4fc367954d5fb60ec34
halwu(吴浩麟) <halwu@tencent.com>
2017-08-24 14:59:57 +0800

    fix: 构建同构应用

--- codes/3.9构建同构应用/http_server.js
+++ codes/3.9构建同构应用/http_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { render } = require('./dist/bundle_server');
 const app = express();
 
-// 调用构建出的 bundle_server.js 中暴露出的渲染函数，在拼接下 HTML 模版，形成完整的 HTML 文件
+// 调用构建出的 bundle_server.js 中暴露出的渲染函数，再拼接下 HTML 模版，形成完整的 HTML 文件
 app.get('/', function (req, res) {
   res.send(`
 <html>
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 </head>
 <body>
 <div id="app">${render()}</div>
-<!--导入 webpack 输出的 JS 文件-->
+<!--导入 webpack 输出的用于浏览器端渲染的 JS 文件-->
 <script src="./dist/bundle_browser.js"></script>
 </body>
 </html>
