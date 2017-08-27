21eb0a674372dbb8e630fc99ee3ac99f6b21e866
halwu(吴浩麟) <halwu@tencent.com>
2017-08-27 12:23:57 +0800

    fix: 构建 Npm 模块

--- codes/3.11构建Npm库/webpack.config.js
+++ codes/3.11构建Npm库/webpack.config.js
@@ -9,9 +9,10 @@ module.exports = {
     filename: 'index.js',
     // 输出文件的存放目录
     path: path.resolve(__dirname, 'lib'),
-    // 输出的代码符合 CommonJS 模块化规范
+    // 输出的代码符合 CommonJS 模块化规范，以供给其它模块导入使用。
     libraryTarget: 'commonjs2',
   },
+  // 通过正则命中所有以 react 或者 babel-runtime 开头的模块
   // 这些模块使用外部的，不能被打包进输出的代码里
   externals: /^(react|babel-runtime)/,
   module: {
