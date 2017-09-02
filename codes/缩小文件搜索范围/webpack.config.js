dffd5eadcdb79ded8e58cf39e5c22fc43e3b376f
halwu(吴浩麟) <halwu@tencent.com>
2017-09-02 12:53:03 +0800

    fix: 缩小文件搜索范围

--- codes/缩小文件搜索范围/webpack.config.js
+++ codes/缩小文件搜索范围/webpack.config.js
@@ -14,8 +14,17 @@ module.exports = {
     modules: [path.resolve(__dirname, 'node_modules')],
     // 只采用 main 字段作为入口文件描述字段，以减少搜索步骤
     mainFields: ['main'],
+    // 使用 alias 把导入 react 的语句换成直接使用单独完整的 react.min.js 文件，减少耗时的递归解析操作
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react/dist/react.min.js'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom/dist/react-dom.min.js'),
+    },
+    // 尽可能的减少后缀尝试的可能性
+    extensions: ['js'],
   },
   module: {
+    // 独完整的 `react.min.js` 文件就没有采用模块化，忽略对 `react.min.js` 文件的递归解析和处理
+    noParse: [/react\.min\.js$/],
     rules: [
       {
         test: /\.js$/,
@@ -25,5 +34,4 @@ module.exports = {
       },
     ]
   },
-  devtool: 'source-map' // 输出 source-map 方便直接调试 ES6 源码
 };
