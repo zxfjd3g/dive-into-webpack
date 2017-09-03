5e3f4c20efa97a672cb480c64b967bd93e5791b2
halwu(吴浩麟) <halwu@tencent.com>
2017-09-03 16:25:25 +0800

    fix: 使用 HappyPack

--- codes/使用HappyPack/webpack.config.js
+++ codes/使用HappyPack/webpack.config.js
@@ -17,11 +17,13 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        // 把对 .js 文件的处理转交给 id 为 babel 的 HappyPack 实例
         use: ['happypack/loader?id=babel'],
         // 排除 node_modules 目录下的文件，node_modules 目录下的文件都是采用的 ES5 语法，没必要再通过 Babel 去转换
         exclude: path.resolve(__dirname, 'node_modules'),
       },
       {
+        // 把对 .css 文件的处理转交给 id 为 css 的 HappyPack 实例
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           use: ['happypack/loader?id=css'],
@@ -31,11 +33,14 @@ module.exports = {
   },
   plugins: [
     new HappyPack({
+      // 用唯一的标识符 id 来代表当前的 HappyPack 是用来处理一类特定的文件
       id: 'babel',
+      // 如何处理 .js 文件，用法和 Loader 配置中一样
       loaders: ['babel-loader?cacheDirectory'],
     }),
     new HappyPack({
       id: 'css',
+      // 如何处理 .css 文件，用法和 Loader 配置中一样
       loaders: ['css-loader'],
     }),
     new ExtractTextPlugin({
