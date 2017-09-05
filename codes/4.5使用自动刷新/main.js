addf3f95a57b720c2f6aac8acacb8bcb3fc29a22
halwu <halwu@tencent.com>
2017-09-05 15:26:37 +0800

    拆分自动刷新和模块热替换

--- codes/4.5使用自动刷新/main.js
+++ codes/4.5使用自动刷新/main.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './main.css';
 
 class Button extends Component {
   render() {
-    return <h1>Hello,Webpack 12</h1>
+    return <h1>Hello,Webpack 123</h1>
   }
 }
 
 render(<Button/>, window.document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept();
+}
--- codes/4.5使用自动刷新/webpack.config.js
+++ codes/4.5使用自动刷新/webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         // 排除 node_modules 目录下的文件，node_modules 目录下的文件都是采用的 ES5 语法，没必要再通过 Babel 去转换
         exclude: path.resolve(__dirname, 'node_modules'),
       },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   // 输出 source-map 方便直接调试 ES6 源码
