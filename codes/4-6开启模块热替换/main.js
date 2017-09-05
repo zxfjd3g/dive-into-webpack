f0b35a379ef7e40cf242a327e68c4a022bbcdbed
halwu <halwu@tencent.com>
2017-09-05 19:57:08 +0800

    fix: 开启模块热替换

---
+++ codes/4-6开启模块热替换/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppComponent } from './AppComponent';
+import './main.css';
+
+render(<AppComponent/>, window.document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept('./AppComponent');
+}
