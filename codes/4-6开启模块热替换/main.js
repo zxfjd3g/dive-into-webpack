1fef0e4cf8f732d9267f925a97404405f8eee5ba
halwu <gwuhaolin@icloud.com>
2017-09-11 14:58:07 +0800

    fix: 开启模块热替换

--- codes/4-6开启模块热替换/main.js
+++ codes/4-6开启模块热替换/main.js
@@ -5,6 +5,12 @@ import './main.css';
 
 render(<AppComponent/>, window.document.getElementById('app'));
 
+// 只有当开启了模块热替换时 module.hot 才存在
 if (module.hot) {
-  module.hot.accept();
+  // accept 函数的第一个参数指出当前文件接受哪些子模块的替换，这里只接受 ./AppComponent 这个子模块
+  // 第二个参数用于在新的子模块加载完毕后需要执行的逻辑
+  module.hot.accept(['./AppComponent'], () => {
+    // 重新执行下组建渲染逻辑
+    render(<AppComponent/>, window.document.getElementById('app'));
+  });
 }
