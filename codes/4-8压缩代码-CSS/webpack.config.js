0d97719bf05eaeee9e2e7a29598d753d4401ea4d
wuhaolin <wuhaolin@meituan.com>
2017-09-23 16:20:11 +0800

    fix: 压缩代码

--- codes/4-8压缩代码-CSS/webpack.config.js
+++ codes/4-8压缩代码-CSS/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const {WebPlugin} = require('web-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const DefinePlugin = require('webpack/lib/DefinePlugin');
-const { WebPlugin } = require('web-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -30,36 +28,13 @@ module.exports = {
     ]
   },
   plugins: [
-    // 使用本文的主角 WebPlugin，一个 WebPlugin 对应一个 HTML 文件
+    // 用 WebPlugin 生成对应的 HTML 文件
     new WebPlugin({
       template: './template.html', // HTML 模版文件所在的文件路径
       filename: 'index.html' // 输出的 HTML 的文件名称
     }),
     new ExtractTextPlugin({
       filename: `[name]_[contenthash:8].css`,// 给输出的 CSS 文件名称加上 hash 值
-    }),
-    new DefinePlugin({
-      // 定义 NODE_ENV 环境变量为 production 去除 react 代码中的开发时才需要的部分
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    // 压缩输出的 JS 代码
-    new UglifyJsPlugin({
-      // 最紧凑的输出
-      beautify: false,
-      // 删除所有的注释
-      comments: false,
-      compress: {
-        // 在UglifyJs删除没有用到的代码时不输出警告
-        warnings: false,
-        // 删除所有的 `console` 语句，可以兼容ie浏览器
-        drop_console: true,
-        // 内嵌定义了但是只用到一次的变量
-        collapse_vars: true,
-        // 提取出出现多次但是没有定义成变量去引用的静态值
-        reduce_vars: true,
-      }
     }),
   ],
 };
