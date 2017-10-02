11b84bdfbbaee400fb8083f6cf3a4bb4d711e57d
wuhaolin <wuhaolin@meituan.com>
2017-10-02 15:08:39 +0800

    feat: jsnext:main

--- codes/4-10使用TreeSharking/webpack.config.js
+++ codes/4-10使用TreeSharking/webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
   },
+  resolve: {
+    // 针对 Npm 中的第三方模块优先采用 jsnext:main 中指向的 ES6 模块化语法的文件
+    mainFields: ['jsnext:main', 'browser', 'main']
+  },
   module: {
     rules: [
       {
